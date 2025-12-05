14293f6b143bf8ad57aa221c931de9096eb848bb
Azmir Khan <azmir.azamkhan@gmail.com>
2025-12-05 17:59:52 +0600

    Initial project setup

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+import 'main_app.dart';
+
+void main(){
+
+  runApp( MainApp() );
+}
---
+++ lib/main_app.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+      ),
+      body: Center(
+        child: Text("Hello"),
+      ),
+    );
+  }
+}
+
